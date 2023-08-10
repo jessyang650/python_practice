94b4d927c00d98b52f9c8ec9e7bdef0db24c11c3
Jess Yang <jessyang@Jesss-Mac-mini.local>
2023-08-09 20:45:26 -0700

    add intermediate file

---
+++ chatGPT_intermediate.py
@@ -0,0 +1,15 @@
+# ChatGPT Python Exercises Intermediate
+
+"""
+Write a Python function that takes a list of strings as input and 
+returns a new list containing only the strings that are palindromes 
+(read the same forwards and backwards).
+"""
+
+def return_palindrome(list_of_strings):
+    list_of_palindrome = []
+    for i in list_of_strings:
+        reversed_string = i[::-1]
+        if i.lower() == reversed_string.lower(): 
+            list_of_palindrome.append(i)
+    return list_of_palindrome
