894e9166647bc7dd0fd4736ca705ab8e8b24ac1c
Jess Yang <jessyang@Jesss-Mac-mini.local>
2023-08-10 16:30:48 -0700

    more intermediate

--- chatGPT_intermediate.py
+++ chatGPT_intermediate.py
@@ -13,3 +13,15 @@ def return_palindrome(list_of_strings):
         if i.lower() == reversed_string.lower(): 
             list_of_palindrome.append(i)
     return list_of_palindrome
+
+"""
+Write a Python function that takes a list of words as input and returns
+a dictionary where the keys are the words and the values are the 
+lengths of the corresponding words.
+"""
+
+def return_dictionary(list_of_words):
+    dictionary = {}
+    for string in list_of_words:
+        dictionary[string] = len(string)
+    return dictionary
